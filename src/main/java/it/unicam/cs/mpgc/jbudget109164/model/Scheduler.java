c52609f36d2186f28aca5b35c2903bae7221597f
Michele Cianni <michele.cianni@studenti.unicam.it>
2025-06-18 01:45:23 +0200

    Add Scheduler interface for generating scheduled transactions within a date range

---
+++ src/main/java/it/unicam/cs/mpgc/jbudget109164/model/Scheduler.java
@@ -0,0 +1,21 @@
+package it.unicam.cs.mpgc.jbudget109164.model;
+
+import java.time.LocalDate;
+import java.util.List;
+
+/**
+ * A functional interface representing a schduler that can generate a list of scheduled transactions
+ * based on a defined schedule within a specified date range.
+ */
+@FunctionalInterface
+public interface Scheduler {
+
+    /**
+     * Generates a list of transactions based on the scheduled transactions within the specified date range.
+     *
+     * @param from the start date of the range
+     * @param to the end date of the range
+     * @return a list of transactions generated for the specified date range
+     */
+    List<ScheduledTransaction> getUpcomingTransactions(LocalDate from, LocalDate to);
+}
