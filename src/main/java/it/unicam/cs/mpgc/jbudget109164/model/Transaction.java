607cf1894ab837aa5acb951174c928ae1a85b79b
Michele Cianni <michele.cianni@studenti.unicam.it>
2025-06-18 01:42:13 +0200

    Add Transaction interface for financial transaction representation

---
+++ src/main/java/it/unicam/cs/mpgc/jbudget109164/model/Transaction.java
@@ -0,0 +1,46 @@
+package it.unicam.cs.mpgc.jbudget109164.model;
+
+import java.time.LocalDate;
+import java.util.Set;
+import java.util.UUID;
+
+/**
+ * A class implementing this interface represents a financial transaction.
+ */
+public interface Transaction {
+
+    /**
+     * Returns the unique identifier of the transaction.
+     *
+     * @return the UUID of the transaction
+     */
+    UUID id();
+
+    /**
+     * Returns the description of the transaction.
+     *
+     * @return the description of the transaction
+     */
+    String description();
+
+    /**
+     * Returns the amount of the transaction.
+     *
+     * @return the amount of the transaction
+     */
+    double amount();
+
+    /**
+     * Returns the date of the transaction.
+     *
+     * @return the date of the transaction
+     */
+    LocalDate date();
+
+    /**
+     * Returns the tags associated with the transaction.
+     *
+     * @return a set of tags associated with the transaction
+     */
+    Set<Tag> tags();
+}
