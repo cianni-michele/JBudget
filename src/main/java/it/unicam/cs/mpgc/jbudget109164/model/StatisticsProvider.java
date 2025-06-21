312b20c46230a9ff9b3366ea1c793ba730f28475
Michele Cianni <michele.cianni@studenti.unicam.it>
2025-06-21 16:03:45 +0200

    Enhance StatisticsProvider to support computation over a Period and add default method for date range

--- src/main/java/it/unicam/cs/mpgc/jbudget109164/model/StatisticsProvider.java
+++ src/main/java/it/unicam/cs/mpgc/jbudget109164/model/StatisticsProvider.java
@@ -1,5 +1,6 @@
 package it.unicam.cs.mpgc.jbudget109164.model;
 
+import it.unicam.cs.mpgc.jbudget109164.model.transaction.Period;
 import it.unicam.cs.mpgc.jbudget109164.repository.DataManager;
 
 import java.time.LocalDate;
@@ -11,6 +12,7 @@ import java.util.Map;
  * Implementations of this interface should provide the logic to aggregate transaction amounts
  * by tags for the specified period.
  */
+@FunctionalInterface
 public interface StatisticsProvider {
 
     /**
@@ -21,5 +23,16 @@ public interface StatisticsProvider {
      * @param to the end date of the range
      * @return a map where keys are tag names and values are the total amounts for each tag
      */
-    Map<String, Double> compute(DataManager data, LocalDate from, LocalDate to);
+    default Map<String, Double> compute(DataManager data, LocalDate from, LocalDate to) {
+        return compute(data, Period.of(from, to));
+    }
+
+    /**
+     * Computes statistics for the given data manager within the specified period.
+     *
+     * @param data   the data manager containing transactions and tags
+     * @param period the period for which to compute statistics
+     * @return a map where keys are tag names and values are the total amounts for each tag
+     */
+    Map<String, Double> compute(DataManager data, Period period);
 }
