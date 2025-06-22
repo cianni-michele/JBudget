a257518cd727c34da92e9efa659256194a1874dd
Michele Cianni <michele.cianni@studenti.unicam.it>
2025-06-22 10:57:44 +0200

    Add BudgetPlanTypeAdapter for JSON serialization and deserialization of BudgetPlan objects

---
+++ src/test/java/it/unicam/cs/mpgc/jbudget109164/repository/serialization/BudgetPlanTypeAdapterTest.java
@@ -0,0 +1,13 @@
+package it.unicam.cs.mpgc.jbudget109164.repository.serialization;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+//TODO: Implement tests for BudgetPlanTypeAdapter
+class BudgetPlanTypeAdapterTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
