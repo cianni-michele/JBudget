c05a276bed8b4c6a37c40d13509816a643a97b81
Michele Cianni <michele.cianni@studenti.unicam.it>
2025-06-24 16:17:17 +0200

    Add tests for YearMonthSerializer serialization and deserialization

--- src/test/java/it/unicam/cs/mpgc/jbudget109164/repository/serialization/YearMonthSerializerTest.java
+++ src/test/java/it/unicam/cs/mpgc/jbudget109164/repository/serialization/YearMonthSerializerTest.java
@@ -1,13 +1,42 @@
 package it.unicam.cs.mpgc.jbudget109164.repository.serialization;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.YearMonth;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-//TODO: Implement tests for YearMonthTypeAdapter
 class YearMonthSerializerTest {
+
+    private Gson gson;
 
     @BeforeEach
     void setUp() {
+        gson = new GsonBuilder()
+                .registerTypeAdapter(YearMonth.class, new YearMonthSerializer())
+                .create();
+    }
+
+    @Test
+    void deserialize() {
+        String json = "\"2025-10\"";
+
+        YearMonth actual = gson.fromJson(json, YearMonth.class);
+        YearMonth expected = YearMonth.of(2025, 10);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void serialize() {
+        YearMonth yearMonth = YearMonth.of(2025, 10);
+
+        String actual = gson.toJson(yearMonth, YearMonth.class);
+        String expected = "\"2025-10\"";
+
+        assertEquals(expected, actual);
     }
 }
